7adab3e112d19e399ac5ca5417a4b50e0f273233
Sri Hari Karthick N <sriharikarthikn@gmail.com>
2024-09-13 15:50:48 +0100

    Scaffolded a new project with documentation.

---
+++ src/main/java/com/codebykarthick/sample/flumtree/FlumtreeApplication.java
@@ -0,0 +1,13 @@
+package com.codebykarthick.sample.flumtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlumtreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlumtreeApplication.class, args);
+	}
+
+}
